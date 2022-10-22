193ecb171610008163304e5bf49d687002e0dbd6
SirKribo <96794907+SirKribo@users.noreply.github.com>
2022-10-22 13:01:30 -0300

    Infinite Ammo script
    Fixed, tested, working; There's two debug.log to show on console when the effect was activated, and when it ends

--- trickgames scifi/Assets/Scripts/InfiniteAmmoTrigger.cs
+++ trickgames scifi/Assets/Scripts/InfiniteAmmoTrigger.cs
@@ -4,42 +4,46 @@ using UnityEngine;
 public class InfiniteAmmoTrigger : MonoBehaviour
 {
 
-
-    //------------------------------------------------UNTESTED------------------------------------------------UNTESTED------------------------------------------------UNTESTED------------------------------------------------UNTESTED------------------------------------------------//
-
-                                                                //This script was made for if the Infinite Ammo ability is an item that the player picks up
     private float effectDuration = 20f;
     private bool effectActive = false;
-    private Gun currentGun;
+    private GameObject currentGun;
+    private GameObject weaponSwitcher;
 
-    private void OnCollisionEnter(Collision collision)                  //this method is if the infinite ammo powerup is an item the player picks up
+    private void Start()
     {
-        currentGun = collision.gameObject.GetComponent<Gun>();          //assigning the current gun to a variable
-        currentGun.infiniteAmmo = true;                                 //straight up just changing the value of the infiniteAmmo variable
-        effectActive = true;                                            //assigning true to the boolean used to end the "CheckGun" method
-        StartCoroutine(InfiniteAmmoTimer());                            //calling the timer to disable the effect
-        CheckGun();                                                     //calling a method for if the gun the player is holding changed, so the effect can stay
-
+        StartCoroutine(InfiniteAmmoTimer());
     }
 
     IEnumerator InfiniteAmmoTimer()
     {
-        yield return new WaitForSeconds(effectDuration);                //the time will count down, then it'll proceed
-        effectActive = false;                                           //ends the loop of the "CheckGun" method
-        currentGun.infiniteAmmo = false;                                //ends the effect
+        yield return new WaitUntil(() => Input.GetButtonDown("Skill1"));                                            //Wait for the player input of Skill1; default value given is "t", can be changed in the Project Settings -> Input Manager
+        effectActive = true;                                                                                        //this is used for the coroutine that checks if the gun is changed
+
+        currentGun = FindObjectOfType<WeaponSwitcher>().gameObject.GetComponentInChildren<Gun>().gameObject;
+        currentGun.GetComponent<Gun>().InfiniteAmmoChanger();
+
+        StartCoroutine(CheckGun());
+        Debug.Log("Countdown started...");
+        yield return new WaitForSeconds(effectDuration);                                                            //the time will count down, then it'll proceed
+        Debug.Log("Countdown finished!");
+
+        StopCoroutine(CheckGun());
+        effectActive = false;                                                                                       //ends the loop of the "CheckGun" method
+        currentGun.GetComponent<Gun>().InfiniteAmmoChanger();                                                       //ends the effect
     }
 
-    void CheckGun()
+    IEnumerator CheckGun()
     {
-        GameObject gunCheck;                                            //creates the comparative variable
-        while (effectActive)                                            //effectActive is set to true before the method is started
+        GameObject gunCheck;                                                                                             //creates the comparative variable
+        while (effectActive)                                                                                             //effectActive is set to true before the method is started
         {
-            gunCheck = FindObjectOfType<Gun>().gameObject;              //comparative variable takes the gameobject of the current active "Gun"
-            if (currentGun.gameObject != gunCheck)                      //simple question for when the gun that was active when the power up was enabled, is different from the current gun in the scene
+            yield return new WaitUntil(() => Input.GetAxis("Mouse ScrollWheel") != 0f);
+            gunCheck = FindObjectOfType<WeaponSwitcher>().gameObject.GetComponentInChildren<Gun>().gameObject;           //comparative variable takes the gameobject of the current active "Gun"
+            if (currentGun != gunCheck && effectActive)                                                                  //when the gun that was active when the power up was enabled is different from the current gun in the scene, and the effect is still active
             {
-                currentGun.infiniteAmmo = false;                        //disables the infiniteAmmo variable for that gun, so that if the effect ends, and the player changes gun, it won't stay with infinite ammo
-                currentGun = gunCheck.GetComponent<Gun>();              //assigning the new gun to the variable
-                currentGun.infiniteAmmo = true;                         //enabling the infinite ammo effect for the new gun
+                currentGun.GetComponent<Gun>().InfiniteAmmoChanger();                                                    //disables the infiniteAmmo variable for that gun, so that if the effect ends, and the player changes gun, it won't stay with infinite ammo
+                currentGun = gunCheck;                                                                                   //assigning the new gun to the variable
+                currentGun.GetComponent<Gun>().InfiniteAmmoChanger();                                                    //enabling the infinite ammo effect for the new gun
             }
         }
     }
